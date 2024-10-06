d23ebcaca6c84564119cb6af63447d1cae27694b
Arianvba24 <153762786+Arianvba24@users.noreply.github.com>
2024-10-06 20:48:25 +0200

    Update spider_testing2.py

--- spider_testing2.py
+++ spider_testing2.py
@@ -109,7 +109,7 @@ class AsyncSpiderFunctions:
         async with httpx.AsyncClient() as client:
 
 
-            browser = mechanicalsoup.StatefuBrowser()
+            browser = mechanicalsoup.StatefulBrowser()
             browser.add_headers({
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
